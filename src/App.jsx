8721d964f61573635736caff6dccf621208bb2dd
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-03-31 23:02:39 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import conf from './Conf/Conf'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-gray-500'> A blog App with AppWrite </h1>
+    </>
+  )
+}
+
+export default App
