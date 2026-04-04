8284df668fd015637510657dd67758ea3a45d1f1
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-04-04 17:41:07 +0530

    feat : added loading fun and dispatched RTK actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import conf from './Conf/Conf'
+import { Header, Footer } from './Components/index'
+import { useDispatch } from 'react-redux'
+import authService from './Appwrite/Auth'
+import { login, logout } from './Store/AuthSlice'
 
 function App() {
 
-  return (
-    <>
-      <h1 className='bg-gray-500'> A blog App with AppWrite </h1>
-    </>
-  )
+  const[ loading , setLoading ] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+      authService.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login(userData))
+        }
+        else{
+          dispatch(logout())
+        }
+      })
+      .finally(
+        setLoading(false)
+      )
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex felx-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* TODO : <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
