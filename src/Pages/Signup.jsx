2d639ea99ebe8f3f939e3a8487dab85188f2e097
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-04-26 01:43:28 +0530

    feat: added pages

---
+++ src/Pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Signup as SignupForm} from '../Components/index'
+function Signup() {
+  return (
+    <div className='py-8'>
+        <SignupForm/>
+    </div>
+  )
+}
+
+export default Signup
