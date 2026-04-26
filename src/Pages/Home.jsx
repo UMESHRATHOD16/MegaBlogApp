57b96d19989a7b518f78103d6ca78dcdc4e74877
UMESHRATHOD16 <umeshhhhhrathod@gmail.com>
2026-04-26 22:59:13 +0530

    feat : added Home Page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import service from '../Appwrite/Config'
+import { PostForm,PostCard, Container } from '../Components'
+
+function Home() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+        service.getPosts().then((posts) => {
+            if(posts){
+                setPosts(posts.documents)
+            }
+        })
+    },[])
+    
+    if(posts.length === 0){
+        return(
+        <Container>
+            <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Login to read posts
+                            </h1>
+                        </div>
+            </div>
+        </Container>)
+    }
+    return (
+        <>
+            <div className='w-full py-8'>
+                <Container>
+                    <div className='flex flex-wrap'>
+                        {posts.map((post) => (
+                            <div key={post.$id} className='p-2 w-1/4'>
+                                <PostCard {...post} />
+                            </div>
+                        ))}
+                    </div>
+                </Container>
+            </div>
+        </>
+    )
+}
+
+export default Home
